d09606601d50f01f47a64bf329cf461fc7e8c455
Matthew Lewis <matt@mplewis.com>
2014-06-20 16:59:34 -0700

    Return heating data; heat/cool on heater pin based on thermometer; poll thermometer at worst every 10 seconds

--- sous_vide_arduino/sous_vide_arduino.ino
+++ sous_vide_arduino/sous_vide_arduino.ino
@@ -1,11 +1,15 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is plugged into port 2 on the Arduino
+// Thermometer is connected to pin 2
 #define ONE_WIRE_BUS 2
+
+// Powerswitch Tail is connected to pin 3
+#define HEATER_PIN 3
 
 // State variables
 bool enabled = false;
+bool heating = false;
 byte targetTempF = 0;
 
 // Setup a oneWire instance to communicate with OneWire devices
@@ -19,15 +23,33 @@ void setup()
 {
     Serial.begin(57600);
     sensors.begin();
+    pinMode(HEATER_PIN, OUTPUT);
+    digitalWrite(HEATER_PIN, 0);
 }
 
 void loop()
 {
+    // Get current temperature
+    sensors.requestTemperatures();
+    int currentTempF = (int) sensors.getTempFByIndex(0);
+    
+    // Turn heater on or off based on current temperature
+    if (currentTempF <= targetTempF + 1) {
+        // Turn on heater when currentTempF is less than 2 degrees above targetTempF
+        heating = 1;
+        
+    } else {
+        // Turn off heater when currentTempF is 2+ degrees above targetTempF
+        heating = 0;
+    }
+    digitalWrite(HEATER_PIN, heating);
+
     // Sleep if there are no incoming commands
     if (!Serial.available()) {
-        Bean.sleep(4294967294);
+        Bean.sleep(10000);
     }
 
+    // Process incoming commands
     Bean.setLed(0, 0, 255);
 
     byte cmd = Serial.read();
@@ -37,11 +59,12 @@ void loop()
         // Send the command to get temperatures
         sensors.requestTemperatures();
 
-        // Return 0x00, current temp, target temp, enabled, 0xFF
+        // Return 0x00, current temp, target temp, enabled, heating, 0xFF
         Serial.write(0x00);
-        Serial.write((int) sensors.getTempFByIndex(0));
+        Serial.write(currentTempF);
         Serial.write(targetTempF);
         Serial.write(enabled ? 0x01 : 0x00);
+        Serial.write(heating ? 0x01 : 0x00);
         Serial.write(0xFF);
 
     } else if (cmd == 0x01) {
@@ -78,5 +101,4 @@ void loop()
     }
 
     Bean.setLed(0, 0, 0);
-
 }
